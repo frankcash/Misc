9e5f8951457ba4bbb0fcc29c7b9782132fc949f2
frankcash <cfcash@mtu.edu>
2014-03-18 22:08:54 -0400

    Small changes

--- practiceServer.js
+++ practiceServer.js
@@ -2,9 +2,9 @@
 // to make it work type into your terminal "node practiceServer.js"
 // go to http://localhost:8080/
 
-var http = require('http'); // starts a jank http server
+var http = require('http');
 
-http.createServer(function(request, response){
+http.createServer(function(request, response){  // starts a jank server
 
 	response.writeHead(200); // gives it a successful status
 	response.write("Doge is running.");
