ed3c94f167f3b3f2da3e922104b41a4ec873633e
frankcash <cfcash@mtu.edu>
2014-03-23 11:52:29 -0400

    failing at getting anything to display, but pushing before lunch

--- MySite/helper.js
+++ MySite/helper.js
@@ -1,3 +1,5 @@
-var div = document.getElementById('main-content');
-
-div.innerHTML = div.innerHTML + 'Extra stuff';
+//var div = document.getElementById('.main-content');
+$( document ).ready(function() {
+  console.log( "ready!" );
+});
+//div.innerHTML ='<p> this is a test holy shit holy shit, I hope this works</p>';
