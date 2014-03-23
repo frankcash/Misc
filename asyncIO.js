7bc5fb83e9af416a305c8e41f18f505d66fb0456
frankcash <cfcash@mtu.edu>
2014-03-22 21:58:29 -0400

    adds comments, fixes var names.

--- asyncIO.js
+++ asyncIO.js
@@ -1,8 +1,10 @@
+// takes a filepath for the third arguement 
+// counts the amount of times something appears.
+
 var fs =require('fs')
 var file = process.argv[2]
 
-
 fs.readFile(file, function (err, contents) {
-  var lines = contents.toString().split('\n').length
-  console.log(lines-1)
+  var count = contents.toString().split('var').length
+  console.log(count-1)
 })
