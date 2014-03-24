900e330f0c67a9e713c82fd99454090a4df2fd6a
frankcash <cfcash@mtu.edu>
2014-03-24 10:22:31 -0400

    moved functions around.

--- MySite/public/helper.js
+++ MySite/public/helper.js
@@ -1,11 +1,10 @@
-$( document ).ready(function() {
-  console.log( "ready!" );
-});
-// make a new function for this
-
 $.getJSON('/scrape', function(data){
-  console.log(data);
+  // console.log(data);
   for(var i = 0; i<30;i++){
     $("<p>" + data[i].rank + ". " + "<a href=" + data[i].url + ">" + data[i].title + "</a></p>").appendTo("#helper")
   }
+});
+$( document ).ready(function() {
+  // console.log( "ready!" );
+  
 });
