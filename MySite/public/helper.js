e3e85aa548196a8273ca1f1f1b1e5e8866485ae7
frankcash <cfcash@mtu.edu>
2014-03-25 22:11:57 -0400

    took out unnecessary stuff, will be working on later.

--- MySite/public/helper.js
+++ MySite/public/helper.js
@@ -1,10 +1,13 @@
-$.getJSON('/scrape', function(data){
+$.getJSON('/scrape', function(data){ // gets the JSON info
   // console.log(data);
   for(var i = 0; i<30;i++){
-    $("<p>" + data[i].rank + ". " + "<a href=" + data[i].url + ">" + data[i].title + "</a></p>").appendTo("#helper")
+    $("<p><b>" + data[i].rank + ".</b> " + "<a href=" + data[i].url + ">" + data[i].title + "</a></p>").appendTo("#helper")
   }
 });
 $( document ).ready(function() {
   // console.log( "ready!" );
-  
+  // $("b").mouseover(function(){
+  //  $("<p>" + data[i].rank + "</p>")
+  // });
+
 });
