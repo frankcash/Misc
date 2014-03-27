e820f22e9a2da37b88511853bd7d1ef21bafb017
frankcash <cfcash@mtu.edu>
2014-03-27 13:27:38 -0400

    this commit adds nothing, i just deleted the file and changed the spacing because i'm retarded.

--- readData.js
+++ readData.js
@@ -2,15 +2,16 @@ var fs = require('fs');
 
 function get_line(filename, line_no, callback) {
     fs.readFile(filename, function (err, data) {
-      if (err) throw err;
-        var lines = data.toString('utf-8').split("\n");
-      if(+line_no > lines.length){
-        return callback('File end reached without finding line', null); // if no lines
-      }
-      callback(null, lines[+line_no]); // call back
-    });
+		      if (err) throw err;
+		        var lines = data.toString('utf-8').split("\n");
+		      if(+line_no > lines.length){
+					        return callback('File end reached without finding line', null); // if no lines
+					      }
+		      callback(null, lines[+line_no]); // call back
+		    });
 }
 
 get_line('sampleNumbers.txt', 1, function(err, line){ // calls the function on the defined file
   console.log('The line: ' + line);
 })
+
