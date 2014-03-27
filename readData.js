5c383eb86aa71b256aae49d745db9496dee648d9
frankcash <cfcash@mtu.edu>
2014-03-26 23:08:06 -0400

    changes ile to display what is on a certain line

--- readData.js
+++ readData.js
@@ -1,15 +1,16 @@
-fs = require('fs'); // imports modules
+var fs = require('fs');
 
-var content;
-
-fs.readFile('sampleNumbers.txt', 'utf8', function(err,data){
-	if(err){
-		return console.log(err);
-	}
-	content = data;
-	processFile(); // gives it call back
-});
-
-function processFile(){
-	console.log(content[0]);
+function get_line(filename, line_no, callback) {
+    fs.readFile(filename, function (err, data) {
+      if (err) throw err;
+        var lines = data.toString('utf-8').split("\n");
+      if(+line_no > lines.length){
+        return callback('File end reached without finding line', null); // if no lines
+      }
+      callback(null, lines[+line_no]); // call back
+    });
 }
+
+get_line('sampleNumbers.txt', 1, function(err, line){ // calls the function on the defined file
+  console.log('The line: ' + line);
+})
