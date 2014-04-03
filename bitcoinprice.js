3d2020057b2e3f6ef45f7e9efa5dbdaf4441a0dd
frankcash <cfcash@mtu.edu>
2014-04-02 22:51:14 -0400

    close enough for now.

--- bitcoinprice.js
+++ bitcoinprice.js
@@ -6,8 +6,24 @@ var cheerio = require('cheerio');
 
 var app = express();
 
+var metadataArray = [ ];
+
+request('http://bitcoincharts.com/', function(error, response, page){
+  if(!error && response.statusCode == 200){
+    var $ = cheerio.load(page); // puts the html in the parser
+    $('td.right').each(function(i, elements){
+      var a=$(this).prev();
+      var price = a.text();
+      metadataArray.push(price);
+    });
+
+  } // end of if statement
+}); // end of function
+
+
+
 app.get('/', function(req, res){
-  res.send('string string string');
+  res.send(metadataArray);
 });
 
 app.listen(3000);
