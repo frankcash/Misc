1952e8e93a20e506d513be5cbe4e8c62614c2b3b
frankcash <cfcash@mtu.edu>
2014-04-02 22:37:14 -0400

    sets up server.

--- bitcoinprice.js
+++ bitcoinprice.js
@@ -1,3 +1,13 @@
+// scrapes info from http://bitcoincharts.com/
+
 var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
+
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('string string string');
+});
+
+app.listen(3000);
