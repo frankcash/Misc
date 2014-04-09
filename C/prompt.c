d654a8353d128a584c9023ed721d500fb0649c7a
Frank Cash <cfcash@mtu.edu>
2014-04-09 11:29:17 -0400

    changed it to focus on unix

--- C/prompt.c
+++ C/prompt.c
@@ -1,31 +1,6 @@
-#import <stdio.h>
-#import <stdlib.h>
-
-#ifdef _WIN32
-
-#include <string.h>
-
-static char buffer[2048];
-
-/*fake readline function*/
-char* readline(char* prompt){
-	fputs(prompt, stdout);
-	fgets(buffer, 2048, stdin);
-	char* cpy=malloc(strlen(buffer)+1);
-	strcpy(cpy, buffer);
-	cpy[strlen(cpy)-1]='\0';
-	return cpy;
-}
-
-/*fake add history function*/
-void add_history(char* unused){}
-
-/*otherwise include the editline headers*/
-#else
-
+#include <stdio.h>
+#include <stdlib.h>
 #include <editline/readline.h>
-
-#endif
 
 int main(int argc, char** argv){
 	puts("Lispy Version 0.0.0.0.1");
