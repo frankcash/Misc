132e11fb02c98e1563d74eaffbd8b731e8bd0e8f
Frank Cash <cfcash@mtu.edu>
2014-04-19 01:22:06 -0400

    nooooooooo

--- fileIOtest.py
+++ fileIOtest.py
@@ -1,3 +1,10 @@
 #gets the number of lines in a file
 num_lines = sum(1 for line in open('sampleNumbers.txt'))
-print(num_lines)
+
+my_file = open("sampleNumbers.txt", "r+")
+i=0
+while i<num_lines:
+    print(str(my_file.readline()))
+    i+=1
+
+my_file.close()
