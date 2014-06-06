e05b73d7b8b3f1ba7df978e9cf23c32ca6cd2e3d
frankcash <cfcash@mtu.edu>
2014-06-06 01:57:27 -0400

    Some file management

--- sketch_apr06j/sketch_apr06j.ino
+++ sketch_apr06j/sketch_apr06j.ino
@@ -16,19 +16,23 @@ void setup() {
   lcd.print("");
 }
 
-void loop() { // displays succeeding numbers in the fibonacci sequence
-  int x=1;
-  int y=1;
-  int runningTotal = 0;
-  for(int i=1; i>0; i++){
-    runningTotal = x+y;
-    x=y;
-    y=runningTotal;
-    lcd.print(runningTotal);
-    delay(500);
-    lcd.clear();
-    delay(500);
+void loop() {
+  int x = random(5);
+  lcd.setCursor(0,1);
+  int randNumber = random(100); // gets a new rand
+  int wordNum = randNumber -x;
+  lcd.print(randNumber-x);
+  delay(500);
+  lcd.clear();  // clears then runs after delay
+  delay(500);
+  x++;
+  if(wordNum>50){
+    lcd.print("potato");
+  }else if(wordNum<50){
+    lcd.print("knishes");
   }
+  delay(500);
+  
+  lcd.clear();  // clears then runs after delay
+   delay(500);
 }
-
-
