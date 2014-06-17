b6595388df4852e68f6847f28e47b9b0e8980e24
frankcash <cfcash@mtu.edu>
2014-06-16 21:43:08 -0400

    New and improved rest with nodejs+mongoose+mongodb

--- Angular_Test_Cases/Example3/app.js
+++ Angular_Test_Cases/Example3/app.js
@@ -1,69 +1,111 @@
+// Load required packages
 var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-// Database
-var mongo = require('mongoskin');
-var db = mongo.db("mongodb://localhost:27017/example3", {native_parser:true});  // directs to the db
+var Beer = require('./models/beer');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// Connect to the beerlocker MongoDB
+mongoose.connect('mongodb://localhost:27017/beerlocker');
 
+// Create our Express application
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// Use the body-parser package in our application
+app.use(bodyParser());
 
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Use environment defined port or 3000
+var port = process.env.PORT || 3000;
 
-// Make our db accessible to our router
-app.use(function(req,res,next){
-    req.db = db;
-    next();
+// Create our Express router
+var router = express.Router();
+
+// Initial dummy route for testing
+// http://localhost:3000/api
+router.get('/', function(req, res) {
+  res.json({ message: 'You are running dangerously low on beer!' });
 });
 
+// Create a new route with the prefix /beers
+var beersRoute = router.route('/beers');
 
-app.use('/', routes);
-app.use('/users', users);
+// Create endpoint /api/beers for POSTS
+beersRoute.post(function(req, res) {
+  // Create a new instance of the Beer model
+  var beer = new Beer();
 
-/// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  // Set the beer properties that came from the POST data
+  beer.name = req.body.name;
+  beer.type = req.body.type;
+  beer.quantity = req.body.quantity;
+
+  // Save the beer and check for errors
+  beer.save(function(err) {
+    if (err)
+      res.send(err);
+
+    res.json({ message: 'Beer added to the locker!', data: beer });
+  });
 });
 
-/// error handlers
+// Create endpoint /api/beers for GET
+beersRoute.get(function(req, res) {
+  // Use the Beer model to find all beer
+  Beer.find(function(err, beers) {
+    if (err)
+      res.send(err);
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+    res.json(beers);
+  });
+});
+
+// Create a new route with the /beers/:beer_id prefix
+var beerRoute = router.route('/beers/:beer_id');
+
+// Create endpoint /api/beers/:beer_id for GET
+beerRoute.get(function(req, res) {
+  // Use the Beer model to find a specific beer
+  Beer.findById(req.params.beer_id, function(err, beer) {
+    if (err)
+      res.send(err);
+
+    res.json(beer);
+  });
+});
+
+// Create endpoint /api/beers/:beer_id for PUT
+beerRoute.put(function(req, res) {
+  // Use the Beer model to find a specific beer
+  Beer.findById(req.params.beer_id, function(err, beer) {
+    if (err)
+      res.send(err);
+
+    // Update the existing beer quantity
+    beer.quantity = req.body.quantity;
+
+    // Save the beer and check for errors
+    beer.save(function(err) {
+      if (err)
+        res.send(err);
+
+      res.json(beer);
     });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  });
 });
 
+// Create endpoint /api/beers/:beer_id for DELETE
+beerRoute.delete(function(req, res) {
+  // Use the Beer model to find a specific beer and remove it
+  Beer.findByIdAndRemove(req.params.beer_id, function(err) {
+    if (err)
+      res.send(err);
 
-module.exports = app;
+    res.json({ message: 'Beer removed from the locker!' });
+  });
+});
+
+// Register all our routes with /api
+app.use('/api', router);
+
+// Start the server
+app.listen(port);
+console.log('Insert beer on port ' + port);
