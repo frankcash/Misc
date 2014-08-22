1e59c72faa5c83514e06ea04b55711c36ffa0f38
Frank Cash <cfcash@mtu.edu>
2014-08-21 23:45:10 -0400

    Solution to Project Euler #3

--- CPP/projectEuler3.cpp
+++ CPP/projectEuler3.cpp
@@ -1,28 +1,22 @@
 // The prime factors of 13195 are 5, 7, 13 and 29.
 // What is the largest prime factor of the number 600851475143 ?
+// the last number it prints out is correct (that isn't 1)
 #include <iostream>
 #include <math.h>
 using namespace std;
 
 int main(){
 
-  double num;
+  long num;
 
-  num = 13195;
-  for( double i = 0; i<num; i++){
-    if(fmod(num,i)==0){
-      cout << "factor is: " << i << endl;
-      for( double j = 1; j<i; j++){
-        if(fmod(i,j)!=0){
-           break;
-        }else if(fmod(i,j)==0){
-          cout << "factor is foo: "<< i<< endl;
+  num = 600851475143;
+  for (long i = 2; i <= num; i++) {
+            if (num % i == 0) {
+                num /= i;
+                i--;
+            }
+            cout << num << endl;
         }
-      } // end of second loop
-    } // end of if
-  } // end of first loop
-
-  cout << "num is: " <<  num << endl;
 
   return 0;
 }
