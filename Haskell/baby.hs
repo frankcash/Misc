1ed1ee4cff1f43cd55e7ecc4c4a37d7a81203f90
frankcash <cashc@acm.org>
2014-09-14 19:52:26 -0400

    More notes on Haskell

--- Haskell/baby.hs
+++ Haskell/baby.hs
@@ -11,4 +11,6 @@ simpleSetComprehension = [ x * 2 | x <- [1..10]]
 simpleSetComprehension' = [x * 2 | x <- [1..10], x*2>=12] -- adds a predicate that says must be greater than or equal to 12
 mediumSetComprehension = [ x | x <- [50..100], mod x 7 == 3] -- prints all numbers from 50 to 100 that when divided by 7 have a remainder of 3
 boomBang xs = [ if x< 10 then "Boom!" else "Bang!" | x <- xs, odd x] -- replaces odd numbers with "Boom!" or "Bang!" based upon whether they are less than or greater than 10
-fizzBuzz = [fb x | x <- [1..100]] where fb y | y `mod` 15 ==0 = "FizzBuzz" | y `mod` 3 ==0 = "Fizz" | y `mod` 5 == 0 = "Buzz" | otherwise = show y
+fizzBuzz = [fb x | x <- [1..100]] where fb y | y `mod` 15 ==0 = "FizzBuzz" | y `mod` 3 ==0 = "Fizz" | y `mod` 5 == 0 = "Buzz" | otherwise = show y -- fizzbuzz
+rightTriangles' = [ (a, b, c) | c <- [1..10], a<- [1..c], b <- [1..a], a^2 + b^2 == c^2, a+b+c==24] -- right triangles whose perimeter equals 24
+
