f5503165c776a66f7ee827c735e95ccc38a44c23
frankcash <cashc@acm.org>
2014-09-19 22:18:12 -0400

    adds an example of a "dynamic" url where the output is based upon the path"

--- Haskell/firstScottyProject/Main.hs
+++ Haskell/firstScottyProject/Main.hs
@@ -1,6 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Web.Scotty
-
+import Data.Monoid (mconcat)
+import Network.HTTP.Types -- allows custom headers and shit mane
 main = scotty 3000 $ do
-  get "/" $ do
-    html "Hello World!"
+--  get "/" $ do
+--    html "Hello World!"
+		get "/:word" $ do
+			beam <- param "word"
+			html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
