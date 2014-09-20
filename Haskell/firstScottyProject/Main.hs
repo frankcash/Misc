b6f22c53851c4faba168568dbd9c1c1aa7be70a3
frankcash <cashc@acm.org>
2014-09-19 22:34:13 -0400

    simple JSON send in haskell

--- Haskell/firstScottyProject/Main.hs
+++ Haskell/firstScottyProject/Main.hs
@@ -5,6 +5,10 @@ import Network.HTTP.Types -- allows custom headers and shit mane
 main = scotty 3000 $ do
 --  get "/" $ do
 --    html "Hello World!"
-		get "/:word" $ do
-			beam <- param "word"
-			html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+--		get "/:word" $ do
+--			beam <- param "word"
+--			html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+		get "/foo" $ do
+			json $ object ["test" .= ("ok" :: String)]
+		notFound $ do -- when nothing found, will match to * not found, so be careful on placement
+			text "there is no such route."
