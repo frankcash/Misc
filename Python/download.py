48317e9a49a5919e2e7fa199dbb6f5103bf60daf
frankcash <cashc@acm.org>
2014-11-06 00:58:06 -0500

    Fixes script

--- Python/download.py
+++ Python/download.py
@@ -1,28 +1,28 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import subprocess
 
-#url = "http://fc14.ifca.ai/papers/fc14_submission_152.pdf"
-
-#r = requests.get(url)
-#with open("test.pdf", "wb") as pdf:
-#    try:
-#        pdf.write(r.content)
-#    finally:
-#        pdf.close
 
 origin = requests.get("http://freehaven.net/anonbib")
 soup=BeautifulSoup(origin.text)
 
 results = soup.find_all(href=re.compile("(http).*(pdf)"))
-
+requests.adapters.HTTPAdapter(max_retries=3)
 for link in results:
     url = (link.get('href'))
-    r = requests.get(url)
-    print url
-    fileForUrl = url.split('/')[-1]
-    with open(fileForUrl, "wb") as pdf:
-        try:
-            pdf.write(r.content)
-        finally:
-            pdf.close
+    url = url.strip()
+    try:
+        r = requests.get(url)
+        fileForUrl = url.split('/')[-1]
+        with open(fileForUrl, "wb") as pdf:
+            try:
+                pdf.write(r.content)
+            finally:
+                pdf.close
+    except:
+        print "oops"
+        bashCommand = "rm " + fileForUrl
+        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    finally:
+        print url
