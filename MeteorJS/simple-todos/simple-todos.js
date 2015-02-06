465ca383e3f728f16795b3c593e162881e12f9fc
frankcash <cashc@acm.org>
2015-02-06 00:35:47 -0500

    Basic todo app complete

--- MeteorJS/simple-todos/simple-todos.js
+++ MeteorJS/simple-todos/simple-todos.js
@@ -23,6 +23,15 @@ if (Meteor.isClient) {
     }
   })
 
+  Template.task.events({
+    "click .toggle-checked": function(){
+      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+    "click .delete": function(){
+      Tasks.remove(this._id);
+    }
+  })
+
 
 }
 
