bc3eff6460f2bceb12c386d92a5075520fedb38e
frankcash <cashc@acm.org>
2015-02-05 23:39:37 -0500

    Works on todo

--- MeteorJS/simple-todos/simple-todos.js
+++ MeteorJS/simple-todos/simple-todos.js
@@ -1,16 +1,34 @@
+Tasks = new Mongo.Collection("tasks"); // connects to mongo
+
 if (Meteor.isClient) {
 // this code only runs on the client
   Template.body.helpers({
-    tasks:[
-      {text: "Learn MeteorJS"},
-      {text: "Practice algos"},
-      {text: "Read book"}
-    ]
+    tasks: function (){
+      return Tasks.find({}, {sort:{createdAt: -1}});
+    }
   });
+
+
+  Template.body.events({
+    "submit .new-task" : function (event){ // listens for submit event with anything that has class .new-task
+      var text = event.target.text.value;
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() //current time
+      });
+
+      event.target.text.value = ""; //clears the form
+
+      return false; // prevent default form submit
+    }
+  })
+
+
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+
   });
 }
