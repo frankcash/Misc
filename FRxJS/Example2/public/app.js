7360db59d7a2fd0f80fae70f28d897885df33a5c
frankcash <cashc@acm.org>
2015-04-06 16:13:48 -0400

    Gets simple data from input

--- FRxJS/Example2/public/app.js
+++ FRxJS/Example2/public/app.js
@@ -1,5 +1,15 @@
 $(function(){
 
-  $("input#username").asEventStream('keyup').log();
+  /**
+  * @summary creates event listener for text field to get value on keyup
+  */
+  function textFieldValue(textField){
+      function value(){
+        return textField.val(); // returns the event's value
+      }
+      return textField.asEventStream("keyup").map(value).toProperty(value());// uses map to transform each event
+  }
 
+  username = textFieldValue($("input#username"));
+  fullname = textFieldValue($("input#fullname"));
 });
