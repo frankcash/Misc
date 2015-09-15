f5d6c1cbbeb475448fd84e887734c8ce0697ccac
frankcash <cashc@acm.org>
2015-09-15 14:55:08 -0400

    updates to an es6 function

--- Js_Node/readData.js
+++ Js_Node/readData.js
@@ -1,18 +1,12 @@
 var fs = require('fs');
 
-
-function get_line(filename, line_no, callback) {
+export default function get_line(filename, line_no, callback) {
     fs.readFile(filename, function (err, data) {
-		      if (err) throw err;
-		        var lines = data.toString('utf-8').split("\n");
+		      if (err){ throw err;}
+		        let lines = data.toString('utf-8').split("\n");
 		      if(+line_no > lines.length){
 					        return callback('File end reached without finding line', null); // if no lines
 					      }
 		      callback(null, lines[+line_no]); // call back
 		    });
 }
-// need to nest this function in a for loop to get all the numbers
-get_line('sampleNumbers.txt', 2, function(err, line){ // calls the function on the defined file
-  console.log('The line: ' + line);
-})
-
