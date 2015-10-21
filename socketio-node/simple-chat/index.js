1f8ae8f452e1ca277c02cd68fc3680f06820a01f
frankcash <cashc@acm.org>
2015-10-20 23:02:24 -0400

    Sender no longer receives re-emit
    Also does optimistic update

--- socketio-node/simple-chat/index.js
+++ socketio-node/simple-chat/index.js
@@ -13,7 +13,7 @@ io.on('connection', function(socket){
 
 	socket.on('chat message', function(msg){
 		console.log('message: ' + msg);
-		io.emit('chat message', msg);
+		socket.broadcast.emit('chat message', msg);
 	});
 
 
