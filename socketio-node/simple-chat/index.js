7086ad2626ebb7c9f71bbd331646a5fbd73faf31
frankcash <cashc@acm.org>
2015-10-24 14:25:23 -0400

    Updates

--- socketio-node/simple-chat/index.js
+++ socketio-node/simple-chat/index.js
@@ -1,7 +1,9 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
