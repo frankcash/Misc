d5f4e436795094498153979cacd6be01def783da
Frank Cash <cashc@acm.org>
2015-11-20 12:04:02 -0500

    Update practiceServer.js

--- Js_Node/practiceServer.js
+++ Js_Node/practiceServer.js
@@ -1,5 +1,3 @@
-// sets up a very small http server
-// to make it work type into your terminal "node practiceServer.js"
 // go to http://localhost:8080/
 
 var http = require('http');
@@ -7,9 +5,6 @@ var http = require('http');
 http.createServer(function(request, response){  // starts a jank server
 
 	response.writeHead(200); // gives it a successful status
-	response.write("Doge is running.");
-	setTimeout(function(){
-		response.end("Doge is done.");
-	}, 5000);
+	response.write("Server is running.");
 	
 }).listen(8080);
