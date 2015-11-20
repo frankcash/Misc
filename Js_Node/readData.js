967d9f8439e9e9f82c521ff43cd6be5b1c5c5482
Frank Cash <cfcash@mtu.edu>
2015-11-20 12:33:15 -0500

    updates readData.js

--- Js_Node/readData.js
+++ Js_Node/readData.js
@@ -1,12 +1,16 @@
 var fs = require('fs');
 
-export default function get_line(filename, line_no, callback) {
+function get_line(filename, line_no, callback) {
     fs.readFile(filename, function (err, data) {
 		      if (err){ throw err;}
-		        let lines = data.toString('utf-8').split("\n");
+		      var lines = data.toString('utf-8').split("\n");
 		      if(+line_no > lines.length){
-					        return callback('File end reached without finding line', null); // if no lines
-					      }
-		      callback(null, lines[+line_no]); // call back
+					       return callback('File end reached without finding line', null); // if no lines
+					}
+		      callback(null, lines[+line_no]); // callback
 		    });
 }
+
+get_line("foo.dat", 8, function(data, info){
+  console.log(info);
+});
